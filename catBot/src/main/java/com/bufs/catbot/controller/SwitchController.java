8d0717c000dba8712f0dd6ac0131c903bdefe544
GiDeok <gdtbgl93@gmail.com>
2018-10-02 13:30:05 +0900

    크롤링 수정중

--- catBot/src/main/java/com/bufs/catbot/controller/SwitchController.java
+++ catBot/src/main/java/com/bufs/catbot/controller/SwitchController.java
@@ -188,7 +188,7 @@ public class SwitchController {
 	
 	
 	//매주 월,화,수 오후 12시에 학식 페이지에서 메뉴를 들고와 DB에 넣어주는 스케쥴러
-	@Scheduled(cron = "0 0 12 ? ? 1-3")
+	@Scheduled(cron = "0 35 13 ? ? MON-FRI")
 	public void CrawlMealInfo() {
 			
 		mongoCrawlingService.CrawlMealTable();
