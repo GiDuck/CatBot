4aee000f5573f539a1aec3e579cf8f454bc0e943
GiDeok <gdtbgl93@gmail.com>
2018-10-03 15:05:13 +0900

    xml tagging

--- catBot/src/main/java/com/bufs/catbot/domain/HolidayItemDTO.java
+++ catBot/src/main/java/com/bufs/catbot/domain/HolidayItemDTO.java
@@ -2,16 +2,22 @@ package com.bufs.catbot.domain;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name="item")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class HolidayItemDTO {
 		
+		@XmlElement
 		private String locdate;
+		@XmlElement
 		private String dateKind;
+		@XmlElement
 		private String isHoliday;
+		@XmlElement
 		private String dateName;
+		@XmlElement
 		private int seq;
 		
 		
