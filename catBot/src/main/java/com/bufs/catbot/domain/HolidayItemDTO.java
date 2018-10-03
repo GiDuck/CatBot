0515215b79a5d85e089d4c7dd11bec18ffe630e3
GiDeok <gdtbgl93@gmail.com>
2018-10-03 17:07:07 +0900

    매핑 수정

--- catBot/src/main/java/com/bufs/catbot/domain/HolidayItemDTO.java
+++ catBot/src/main/java/com/bufs/catbot/domain/HolidayItemDTO.java
@@ -1,9 +1,12 @@
 package com.bufs.catbot.domain;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name="item")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class HolidayItemDTO {
 	
 
